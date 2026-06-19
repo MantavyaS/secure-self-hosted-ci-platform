47d84bec3fb1cfcf117626a5961516dfd0001cf7
Mantavya <themantavyasethi@gmail.com>
2026-06-19 14:49:34 -0400

    configured a vpc module

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -2,6 +2,30 @@ provider "aws" {
   region = "us-east-1"
 }
 
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "secure_ci_vpc"
+  cidr = "10.0.0.0/16"
+
+  azs = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  public_subnets = ["10.0.102.0/24", "10.0.103.0/24", "10.0.104.0/24"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+
+  enable_nat_gateway = false
+  single_nat_gateway = false 
+  map_public_ip_on_launch = true
+  
+  tags = {
+    Project     = "Secure_Self_Hosted_CI_Platform"
+    Environment = "dev"
+    Owner       = "Mantavya"
+    Terraform   = "true"
+    Name        = var.instance_name
+  }
+}
+
 data "aws_ami" "ubuntu" {
   most_recent = true
 
