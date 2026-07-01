b5fec4d834dafecdabf252da8f0a825f916c0f91
Mantavya <themantavyasethi@gmail.com>
2026-07-01 19:25:32 -0400

    testing workflow

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -25,7 +25,7 @@ module "vpc" {
   }
 }
 
-# Security groups
+# Security groups - 
 resource "aws_security_group" "secure_ci_sg" {
   name        = "secure-ci-sg"
   description = "security group for the secure self hosted CI platform"
