7a43a1d8992d392d6ece15a5cb7dff77de0bc8eb
Mantavya <themantavyasethi@gmail.com>
2026-06-30 21:33:45 -0400

    minor fix

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -226,5 +226,5 @@ resource "aws_ecr_lifecycle_policy" "secure_ci_ecr" {
 // secrets manager
 
 data "aws_secretsmanager_secret" "github_arc_private_key" {
-  name        = "github-arc-private-key"
+  name = "github-arc-private-key"
 }
