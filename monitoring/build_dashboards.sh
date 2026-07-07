409441342958a6dcb2241b63e477055c69b22b70
Mantavya <themantavyasethi@gmail.com>
2026-07-07 14:34:15 -0400

    Added bash file to build grafana dashboards from the json

---
+++ monitoring/build_dashboards.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -euo pipefail
+
+GRAFANA_URL="http://monitoring-grafana.monitoring"
+DASHBOARD_DIR="/home/ubuntu/projects/secure-self-hosted-ci-platform/monitoring/dashboards"
+
+USER=$(kubectl get secret -n monitoring monitoring-grafana -o jsonpath="{.data.admin-user}" | base64 -d)
+PASS=$(kubectl get secret -n monitoring monitoring-grafana -o jsonpath="{.data.admin-password}" | base64 -d)
+
+for file in "$DASHBOARD_DIR"/*.json; do
+    echo "provisioning $file"
+
+    code=$(curl -s -o /tmp/grafana_response.json -w "%{http_code}" \
+    -u "$USER:$PASS"   \
+    -H "Content-Type: application/json"   \
+    -X POST   \
+    "$GRAFANA_URL/apis/dashboard.grafana.app/v2/namespaces/default/dashboards"   \
+    --data-binary @"$file")
+
+    if [ "$code" == "409" ]; then
+        curl -s -o /tmp/grafana_response.json -w "%{http_code}" \
+        -u "$USER:$PASS"   \
+        -H "Content-Type: application/json"   \
+        -X PUT   \
+        "$GRAFANA_URL/apis/dashboard.grafana.app/v2/namespaces/default/dashboards"   \
+        --data-binary @"$file" | jq
+    
+    else
+        cat /tmp/grafana_response.json | jq
+    
+    fi
+done
+    
